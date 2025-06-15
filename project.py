04e598542f68e52cd78b4adb56509d5228402a24
Kaka <125654767+Kaka-rcw@users.noreply.github.com>
2025-06-15 18:37:46 +0700

    feat: add newfile event listener so that file is sorted automatically

--- project.py
+++ project.py
@@ -1,5 +1,7 @@
 from pathlib import Path
-
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import time
 SUFFIX_MAP = {
     "images": [".jpg", ".png", ".svg", ".gif"],
     "videos": [".mp4", ".mov", ".avi", ".wmv", ".mkv"],
@@ -12,6 +14,14 @@ SUFFIX_MAP = {
 
 def main():
     p = Path("~/dev/test-project").expanduser()
+    watchFileChange(p)
+
+class NewFileHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        if not event.is_directory:
+            handleFile(p)
+
+def handleFile(p):
     for file in p.glob("*"):
         if not file.is_file():
             continue
@@ -22,12 +32,29 @@ def main():
                 break
         else:
             process(file, "etc")
-        
+
 def process(file, tp):
     des_dir = Path(f"~/dev/test-project/{tp}/").expanduser()
     des = des_dir / file.name
     des.parent.mkdir(parents=True, exist_ok=True)
+    index = 1
+    while des.exists():
+        des = des_dir /f"{des.stem}_{index}{des.suffix}"
+        index += 1
     file.rename(des)
+
+def watchFileChange(p):
+    handler = NewFileHandler()
+    observer = Observer()
+    observer.schedule(handler, p, recursive=False)
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
 
 if __name__ == "__main__":
     main()
